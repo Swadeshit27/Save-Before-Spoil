890d8d7b8bf49ad6ea2e6ebe80cf572879112c6a
Keshab Das <kd2534@it.jgec.ac.in>
2024-06-29 00:37:59 +0530

    csv updated

--- client/src/pages/CsvUploader.jsx
+++ client/src/pages/CsvUploader.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-// src/components/CsvUploader.js
-import React, { useState } from "react";
-import Papa from "papaparse";
-import { db } from "../../firebase/firebase";
-import { collection, addDoc } from "firebase/firestore";
-
-const CsvUploader = () => {
-  const [data, setData] = useState([]);
-  console.log(data);
-
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-
-    Papa.parse(file, {
-      header: true,
-      complete: (results) => {
-        setData(results.data);
-        uploadToFirebase(results.data);
-      },
-    });
-  };
-
-  const uploadToFirebase = async (parsedData) => {
-    const collectionRef = collection(db, "your-collection-name");
-    for (const item of parsedData) {
-      try {
-        await addDoc(collectionRef, item);
-        console.log("Document successfully written!");
-      } catch (error) {
-        console.error("Error writing document: ", error);
-      }
-    }
-  };
-
-  return (
-    <div>
-      <h1>Please choose your CSv file</h1>
-      <input type="file" accept=".csv" onChange={handleFileUpload} />
-    </div>
-  );
-};
-=======
 // src/App.js
 import React, { useState } from 'react';
 import Papa from 'papaparse';
@@ -109,6 +66,5 @@ function CsvUploader() {
         </div>
     );
 }
->>>>>>> 025ef502425d67981b905303088ebfd163519392
 
 export default CsvUploader;
